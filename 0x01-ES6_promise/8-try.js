632b9008a34a358b07871dd0ff44fb51df1bff8b
jan-ice17 <judysjewel17@example.com>
2024-10-04 23:19:44 +0300

    file 8 modified

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,23 +1,7 @@
-// This function takes a boolean parameter and returns a promise
-// If the parameter is true, it resolves the promise with a success response
-// If the parameter is false, it rejects the promise with an error message
-export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-      if (success) {
-        resolve({
-          status: 200,
-          body: 'Success',
-        });
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-  }
-  
-  // This function divides two numbers and throws an error if the denominator is zero
-  export function divideFunction(numerator, denominator) {
+export default function divideFunction(numerator, denominator) {
     if (denominator === 0) {
       throw new Error('cannot divide by 0');
+    } else {
+      return numerator / denominator;
     }
-    return numerator / denominator;
   }
